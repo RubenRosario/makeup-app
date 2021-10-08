f250f40b02f5d39bdfeb4c937ef3abdcb073d506
Ruben <rubenmrosario87@gmail.com>
2021-10-08 14:15:59 -0400

    set basic app's funcionallity

--- src/App.js
+++ src/App.js
@@ -1,7 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import CardsContainer from './components/cardsContainer';
+import Pagination from './components/pagination';
+import Layout from './components/layout';
+
 import './App.css';
 
 function App() {
-	return <div className='App'> This works </div>;
+	const [assets, setAssets] = useState([]);
+	const [loading, setLoading] = useState(false);
+	const [currentPage, setCurrentPage] = useState(1);
+	const [assetsPerPage] = useState(10);
+
+	// get data from api on mount
+	useEffect(() => {
+		const fetchAssets = async () => {
+			setLoading(true);
+			const res = await axios.get(
+				'http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline'
+			);
+			setAssets(res.data);
+			setLoading(false);
+		};
+		fetchAssets();
+	}, []);
+
+	// current assets
+	const indexOfLastAsset = currentPage * assetsPerPage;
+	const indexOfFirstAsset = indexOfLastAsset - assetsPerPage;
+	const currentAssets = assets.slice(indexOfFirstAsset, indexOfLastAsset);
+
+	// change page
+	const changePage = (number) => setCurrentPage(number);
+
+	return (
+		<Layout>
+			<CardsContainer currentAssets={currentAssets} loading={loading} />
+			<Pagination
+				assetsPerPage={assetsPerPage}
+				totalAssets={assets.length}
+				paginate={changePage}
+			/>
+		</Layout>
+	);
 }
 
 export default App;
