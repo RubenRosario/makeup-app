4c014f75cd96173a74e7525b8cb5f29c94b484c4
Ruben <rubenmrosario87@gmail.com>
2021-10-11 00:38:20 -0400

    improve card component's style and functionality

--- src/components/card.js
+++ src/components/card.js
@@ -1,19 +1,42 @@
 import React from 'react';
+import ColorPalette from './colorPalette';
+import ReactStars from 'react-rating-stars-component';
+
 import './card.css';
 
 const Card = ({ asset, loading }) => {
-	const { image_link, name, description, price, rating } = asset;
+	const {
+		image_link,
+		name,
+		brand,
+		description,
+		price,
+		product_colors,
+		rating,
+	} = asset;
 	return (
 		<div className='card-container'>
 			<div className='image-container'>
 				<img src={image_link} alt={description} />
 			</div>
-			<div className='name-price-container'>
-				<div className='name'>{name}</div>
-				<div className='price'>${price}</div>
+			<div className='brand'>{brand}</div>
+			<div className='name'>{name}</div>
+			{product_colors ? (
+				<ColorPalette colorArray={product_colors} />
+			) : (
+				<div></div>
+			)}
+			<div className='rating'>
+				<ReactStars
+					count={5}
+					edit={false}
+					value={rating ? rating : 0}
+					color={'#bfbaba'}
+					size={24}
+					activeColor='#ffd700'
+				/>
 			</div>
-			<div className='color'>color</div>
-			<div className='rating'>{rating}</div>
+			<div className='price'>${price}</div>
 		</div>
 	);
 };
