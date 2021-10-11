8d74ada7f1f775885cab379cc66486d083fb1431
Ruben <rubenmrosario87@gmail.com>
2021-10-10 21:06:44 -0400

    improve pagination style and functionality

--- src/components/pagination.js
+++ src/components/pagination.js
@@ -2,24 +2,55 @@ import React from 'react';
 
 import './paginator.css';
 
-const Pagination = ({ assetsPerPage, totalAssets, paginate }) => {
+const Pagination = ({ currentPage, assetsPerPage, totalAssets, paginate }) => {
+	// create an array with all page numbers.
 	const pageNumbers = [];
-
 	for (let i = 1; i < Math.ceil(totalAssets / assetsPerPage); i++) {
 		pageNumbers.push(i);
+	}
+
+	const firstPage = pageNumbers[0];
+	const lastPage = pageNumbers[pageNumbers.length - 1];
+	const previousPage = currentPage - 1;
+	const nextPage = currentPage + 1;
+
+	// filter page numbers to appear in the paginator
+	// (first, last, current, next, and previous)
+	const showingPageNumbers = pageNumbers.filter(
+		(number) =>
+			number === firstPage ||
+			number === lastPage ||
+			number === currentPage ||
+			number === nextPage ||
+			number === previousPage
+	);
+
+	// fill in the gaps between non-adjacent page numbers with '...'
+	if (previousPage - firstPage > 1) {
+		showingPageNumbers.splice(1, 0, '...');
+	}
+	if (lastPage - nextPage > 1) {
+		showingPageNumbers.splice(showingPageNumbers.length - 1, 0, '...');
 	}
 
 	return (
 		<nav>
 			<ul className='page-numbers-container'>
-				Go to page...
-				{pageNumbers.map((number) => (
-					<li className='page-number' key={number}>
-						<a onClick={() => paginate(number)} href='!#'>
-							{number}
-						</a>
-					</li>
-				))}
+				{showingPageNumbers.map((number, index) =>
+					Number(number) ? (
+						<li
+							key={index}
+							className={`page-number ${currentPage === number && 'current'}`}>
+							<a onClick={() => paginate(number)} href='!#'>
+								{number}
+							</a>
+						</li>
+					) : (
+						<li key={index} className={'ellipsis'}>
+							...
+						</li>
+					)
+				)}
 			</ul>
 		</nav>
 	);
