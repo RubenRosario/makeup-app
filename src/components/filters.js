54a358c6f24feff04458fc314af265a51935a09b
Ruben <rubenmrosario87@gmail.com>
2021-10-13 00:39:52 -0400

    add filter component

---
+++ src/components/filters.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+
+import './filters.css';
+
+const Filters = ({ filterHandler }) => {
+	const [inputs, setInputs] = useState({
+		brand: '',
+		minPrice: '',
+		maxPrice: '',
+		productType: '',
+	});
+
+	const handleInputChange = (e) => {
+		const { name, value } = e.target;
+		setInputs({
+			...inputs,
+			[name]: value,
+		});
+	};
+	return (
+		<form>
+			<div className='filters-container'>
+				<input
+					type='text'
+					name='brand'
+					className='brand-filter'
+					placeholder='Brand Name'
+					value={inputs.brand}
+					onChange={handleInputChange}
+				/>
+				<div className='price-limits-container'>
+					<input
+						type='number'
+						name='minPrice'
+						className='lowest-price-limit'
+						placeholder='Min Price'
+						value={inputs.minPrice}
+						onChange={handleInputChange}
+					/>
+					<input
+						type='number'
+						name='maxPrice'
+						className='highest-price-limit'
+						placeholder='Max Price'
+						value={inputs.maxPrice}
+						onChange={handleInputChange}
+					/>
+				</div>
+				<input
+					type='text'
+					name='productType'
+					className='product-type-filter'
+					placeholder='Prod. Type'
+					value={inputs.productType}
+					onChange={handleInputChange}
+				/>
+				<button type='button' onClick={() => filterHandler(inputs)}>
+					Filter
+				</button>
+			</div>
+		</form>
+	);
+};
+
+export default Filters;
