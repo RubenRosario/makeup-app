1636ef019c771fdbd7573d0f509073bebd8560ed
Ruben <rubenmrosario87@gmail.com>
2021-10-13 01:01:39 -0400

    add some comments and minor changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 import CardsContainer from './components/cardsContainer';
 import Pagination from './components/pagination';
 import Layout from './components/layout';
+import { compare, getIndexes } from './utils/utils';
+import Filters from './components/filters';
 
 import './App.css';
 
@@ -10,33 +12,54 @@ function App() {
 	const [assets, setAssets] = useState([]);
 	const [loading, setLoading] = useState(false);
 	const [currentPage, setCurrentPage] = useState(1);
-	const [assetsPerPage] = useState(10);
+	const [assetsPerPage] = useState(10); // hard code 10 assets per page
 
-	// get data from api on mount
 	useEffect(() => {
-		const fetchAssets = async () => {
+		// get data from api on mount
+		(async function fetchAssets() {
 			setLoading(true);
 			const res = await axios.get(
-				'http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline'
+				'http://makeup-api.herokuapp.com/api/v1/products.json'
 			);
-			setAssets(res.data);
+			const data = res.data;
+			data.sort((obj1, obj2) => compare(obj1, obj2, 'rating', 'desc'));
+			setAssets(data);
 			setLoading(false);
-		};
-		fetchAssets();
+		})();
+
+		// restore the previous state of current page
+		const storedCurrentPage = JSON.parse(
+			window.localStorage.getItem('currentPage')
+		);
+		if (storedCurrentPage) {
+			setCurrentPage(storedCurrentPage);
+		}
 	}, []);
 
-	// current assets
-	const indexOfLastAsset = currentPage * assetsPerPage;
-	const indexOfFirstAsset = indexOfLastAsset - assetsPerPage;
-	const currentAssets = assets.slice(indexOfFirstAsset, indexOfLastAsset);
+	// save current page whenever it changes
+	useEffect(() => {
+		window.localStorage.setItem('currentPage', currentPage);
+	}, [currentPage]);
 
-	// change page
+	// get the index for the fist and last asset
+	// then set the assets for the current page
+	const indexes = getIndexes(currentPage, assetsPerPage);
+	const currentAssets = assets.slice(indexes[0], indexes[1]);
+
+	// change pages
 	const changePage = (number) => setCurrentPage(number);
+
+	// Filter list based on brand, price, and/or productType
+	const filterHandler = (filters) => {
+		// not yet implemented
+	};
 
 	return (
 		<Layout>
+			<Filters filterHandler={filterHandler} />
 			<CardsContainer currentAssets={currentAssets} loading={loading} />
 			<Pagination
+				currentPage={currentPage}
 				assetsPerPage={assetsPerPage}
 				totalAssets={assets.length}
 				paginate={changePage}
